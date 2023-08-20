2ed2af861710388502094d7cfac0d61c32e7d4fc
kimhakSeang <104565674+kimhakSeang@users.noreply.github.com>
2023-08-20 17:09:36 +0700

    School-Config

---
+++ SchoolConfiguration/src/test/java/com/school/configuration/SchoolConfiguration/SchoolConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.configuration.SchoolConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
